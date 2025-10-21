c862c41363a4b464c05af6ce11da59700c8fddd1
Brandon <brandon.w.vetter@gmail.com>
2025-10-21 10:55:53 -0700

    added constants

---
+++ constants.py
@@ -0,0 +1,36 @@
+###############################################################################
+#
+# constants.py
+#
+# @Function: The purpose of this file is to define the commonly used varables
+# used for quantum mechanics
+###############################################################################
+
+from enum import Enum
+
+######################
+# Global Varables
+#####################
+
+# Quantum Varables
+
+hbar_J = 1.054e-34 # J*s
+hbar_eV = 6.58e-16 # eV
+h_nobar_eV = 4.135e-15  # in eV
+h_nobar_J = 6.625e-34 # J*s
+m0 = 9.1e-31 # kg
+meff = m0 
+ecoul = 1.6e-19 # C
+epsz = 8.85e-9 # H/m permebilit of free space
+eV2J = 1.6e-19 # Convert to Joules
+J2eV = 1/eV2J # Convert to eV
+G0 = 38.7 # micro S Quantum Conductance
+
+
+# Semiconductor Values
+    # all in m/me
+Si = 1.08
+GaAs = 0.067
+Ge = 0.55
+
+
