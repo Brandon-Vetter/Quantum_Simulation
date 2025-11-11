d504259f63ad6140e3b5548f2b1799b215509faf
Brandon <brandon.w.vetter@gmail.com>
2025-11-11 15:28:54 -0800

    infante well example

---
+++ examples/infante_well.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+sys.path.append("..")
+import quantum
+import simulation
+import particle
+import v_field
+import init_function
+
+sim = simulation.Simulation(dt=8E-17, del_x=.2E-9, sim_size=200)
+sim.init_dft(0, 1000, end=.5)
+time_ip = init_function.Time_gausian_init(Ein=.27, loc=100)
+
+part = particle.Particle()
+sim.init_particle(part, time_ip)
+
+while True:
+    try:
+        steps = int(input("time steps: "))
+    except ValueError:
+        continue
+    
+    sim.run(steps=steps)
+    
+    plt.plot(sim.sim_space, sim.particles[0].prl)
+    plt.plot(sim.sim_space, sim.particles[0].pim)
+    plt.grid()
+    plt.show()
+
